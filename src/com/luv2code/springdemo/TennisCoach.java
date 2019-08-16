b6671e3f25f939b3cdbdb60cc8319b9fc08a320e
aburk3 <aburk3@gmail.com>
2019-08-16 06:54:18 -0500

    Set up the TennisCoach constructor and used the Autowire annotation to connect to the HappyFortuneService

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -1,11 +1,21 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 
 	private FortuneService fortuneService;
+	
+	/*
+	 * Spring will scan for a component that implements FortuneService interface.
+	 * In this case, it will be HappyFortuneService.
+	 */
+	@Autowired
+	public TennisCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
 	
 	@Override
 	public String getDailyWorkout() {
