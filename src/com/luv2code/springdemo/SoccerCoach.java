94fd651495432717cd56385976fea5f8982bae51
aburk3 <aburk3@gmail.com>
2019-08-16 06:49:43 -0500

    Added FortuneService to Coach classes

--- src/com/luv2code/springdemo/SoccerCoach.java
+++ src/com/luv2code/springdemo/SoccerCoach.java
@@ -5,9 +5,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class SoccerCoach implements Coach {
 
+	private FortuneService fortuneService;
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Run faster across the field and kick for a goal!";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
 	}
 
 }
