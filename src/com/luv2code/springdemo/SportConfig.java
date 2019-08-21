eac418fd76978724168ae72f501305999c61a8f5
aburk3 <aburk3@gmail.com>
2019-08-21 06:52:59 -0500

    Finished implementation for reading spring config java class

--- src/com/luv2code/springdemo/SportConfig.java
+++ src/com/luv2code/springdemo/SportConfig.java
@@ -1,11 +1,10 @@
 package com.luv2code.springdemo;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.luv2code.springdemo")
+// @ComponentScan("com.luv2code.springdemo")
 public class SportConfig {
 	
 	// define bean for our sad fortune service
